811113f8a9211bc4a9e45d3eba8bdba8566b3e97
MariusCausemann <mariuscausemann@yahoo.de>
2024-12-15 12:21:26 +0100

    bump pybind

--- cmake/WildMeshingDownloadExternal.cmake
+++ cmake/WildMeshingDownloadExternal.cmake
@@ -40,7 +40,7 @@ endfunction()
 function(wildmeshing_download_pybind11)
     wildmeshing_download_project(pybind11
         GIT_REPOSITORY https://github.com/pybind/pybind11
-        GIT_TAG        085a29436a8c472caaaf7157aa644b571079bcaa
+        GIT_TAG        a2e59f0e7065404b44dfe92a28aca47ba1378dc4
     )
 endfunction()
 
